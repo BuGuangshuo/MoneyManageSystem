eb726745fdbb79d81186d39496ff9d63414e770c
buguangshuo <buguangshuo@laiye-dc.com>
2021-10-22 19:21:48 +0800

    chage: 更改主题色,美化login框样式

--- src/pages/login/components/Right_Area/index.tsx
+++ src/pages/login/components/Right_Area/index.tsx
@@ -26,7 +26,7 @@ export default function RightArea() {
       <div className={styles['login-form-wrap']}>
         <div className={styles['form-wrap']}>
           <div className={styles['form-title']}>
-            Welcome to System
+            welcome
           </div>
           <Form
             name="normal_login"
@@ -38,31 +38,18 @@ export default function RightArea() {
               name="username"
               rules={[{ required: true, message: 'Please input your Username!' }]}
             >
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-            </Form.Item>
-
-            <Form.Item
-              name="username"
-              rules={[{ required: true, message: 'Please input your Username!' }]}
-            >
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-            </Form.Item>
-
-            <Form.Item
-              name="username"
-              rules={[{ required: true, message: 'Please input your Username!' }]}
-            >
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+              <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username" bordered = {false}/>
             </Form.Item>
 
             <Form.Item
               name="password"
               rules={[{ required: true, message: 'Please input your Password!' }]}
             >
-              <Input
+              <Input.Password
                 prefix={<LockOutlined className="site-form-item-icon" />}
                 type="password"
                 placeholder="Password"
+                bordered = {false}
               />
             </Form.Item>
             <Form.Item>
@@ -72,18 +59,17 @@ export default function RightArea() {
 
               <a className="login-form-forgot" href="">
                 Forgot password
-        </a>
+              </a>
             </Form.Item>
 
             <Form.Item>
               <Button type="primary" htmlType="submit" className="login-form-button">
                 Log in
-        </Button>
-        Or <a href="">register now!</a>
+              </Button>
             </Form.Item>
           </Form>
+        </div>
       </div>
-    </div>
     </div >
   )
 }
