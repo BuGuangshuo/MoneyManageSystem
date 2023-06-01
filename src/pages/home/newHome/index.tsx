31f945a3efcb5b3183d904d731b2e6048571c76b
BuGuangshuo <buguangshuo19@163.com>
2023-06-01 18:10:09 +0800

    feat: 增加加入团队pop

--- src/pages/home/newHome/index.tsx
+++ src/pages/home/newHome/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
 
-import { Button, theme, Tour } from 'antd'
+import { Button, theme, Tour, Popover, Input } from 'antd'
 
 import type { TourProps } from 'antd';
 
@@ -13,6 +13,8 @@ import './index.less'
 
 export default function NewHomePage() {
 
+    const [addPopOpen, setAddPopOpen] = useState<boolean>(false);
+
     const ref1 = useRef(null);
     const ref2 = useRef(null);
 
@@ -20,15 +22,43 @@ export default function NewHomePage() {
         token: { colorTextSecondary, colorPrimary },
       } = theme.useToken();
 
+    const hide = () => {
+        setAddPopOpen(false);
+    };
+    
+    const handleOpenChange = (newOpen: boolean) => {
+        setAddPopOpen(newOpen);
+    };
+
+    const getAddPopContent = () => {
+        return (
+            <div>
+                <div className='addGroupInput'><Input placeholder="请输入团队编号"/></div>
+                <div className='addGroupBtn'><Button type='primary'>申请加入</Button></div>
+            </div>
+        )
+    }
+    
   return (
     <div className="newHome-wrap">
         <div className="newHome-left-wrap">
             <div className="newHome-title DingDing" style={{color: colorPrimary}}>Welcome</div>
-            <div className="newHome-desc DingDing" style={{color: colorTextSecondary}}>欢迎使用财政管理系统,您可以在这里详细查看您团队或个人的财政信息、分析数据等。首先您可以创建您的团队或加入已有团队。</div>
+            <div className="newHome-desc DingDing" style={{color: colorTextSecondary}}>欢迎使用财政管理系统,您可以在这里详细查看您团队或个人的财政信息、分析数据等。</div>
+            <div className="newHome-desc DingDing" style={{color: colorTextSecondary}}>首先您可以创建您的团队或加入已有团队。</div>
 
             <div className="newHome-btn-wrap">
                 <div ref={ref1}><ExploreBtn name="创建团队"/></div>
-                <div ref={ref2}><ExploreBtn name="加入团队"/></div>
+                <Popover
+                    content={getAddPopContent()}
+                    overlayClassName="addGroupPop"
+                    title="加入团队"
+                    trigger="click"
+                    placement="bottomRight"
+                    open={addPopOpen}
+                    onOpenChange={handleOpenChange}
+                >
+                   <div ref={ref2}><ExploreBtn name="加入团队"/></div>
+                </Popover>
             </div>
         </div>
 
