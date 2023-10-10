ac98b970e4d26f84e04884deb0e43bd123a454ed
BuGuangshuo <buguangshuo19@163.com>
2023-10-10 18:20:36 +0800

    feat: 新建目标开发

--- Front/src/pages/analysis/target/modal/index.tsx
+++ Front/src/pages/analysis/target/modal/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
-import { theme, InputNumber, Modal, Input, Form, DatePicker } from "antd";
+import { theme, InputNumber, Modal, Input, DatePicker } from "antd";
+import type { DatePickerProps } from "antd";
 
 import "../index.less";
 import {
@@ -17,38 +18,44 @@ export default function TargetModal(props: any) {
   const { open, isShowEdit, reflash, setReflash, onCancel } = props;
 
   const [barWidth, setBarWidth] = useState<number>(1);
+  const [nameVal, setNameVal] = useState<string>("");
+  const [amountVal, setAmountVal] = useState<number>(0);
+  const [dateRangeVal, setDateRangeVal] = useState<any>([]);
+  const [dateVal, setDateVal] = useState<any>(null);
+  const [timeType, setTimeType] = useState<string>("range");
   const [targetParam, setTargetParam] = useState({});
-
-  const [nameForm] = Form.useForm();
-  const [amountForm] = Form.useForm();
-  const [timeForm] = Form.useForm();
 
   const {
     token: {
       colorBorderSecondary,
       colorSuccess,
-      colorText,
+      colorPrimary,
       colorTextSecondary,
       colorTextTertiary,
     },
   } = theme.useToken();
 
+  const onNameChange = (e: any) => {
+    setNameVal(e.target.value);
+  };
+
+  const onAmountChange = (e: any) => {
+    setAmountVal(e);
+  };
+
+  const onRangeChange = (dates: any) => {
+    setDateRangeVal(dates);
+  };
+
+  const onDateChange: DatePickerProps["onChange"] = (date, dateString) => {
+    setDateVal(date);
+  };
+
   const onNext = async () => {
-    console.log(nameForm.getFieldsValue());
     if (barWidth === 3) {
       onClose();
     } else {
       try {
-        const nameParams = await nameForm.validateFields();
-        const amountParams = await amountForm.validateFields();
-        const timeParams = await timeForm.validateFields();
-
-        setTargetParam({
-          nameParams,
-          amountParams,
-          timeParams,
-        });
-
         setBarWidth(barWidth + 1);
       } catch (e) {
         console.error(e);
@@ -65,21 +72,10 @@ export default function TargetModal(props: any) {
   };
 
   const onClose = () => {
-    console.log(amountForm.getFieldsValue());
-
-    amountForm.setFieldsValue({
-      amount: 0,
-    });
-
-    nameForm.setFieldsValue({
-      name: null,
-    });
-
-    timeForm.setFieldsValue({
-      time: null,
-    });
     setBarWidth(1);
-
+    setAmountVal(0);
+    setNameVal("");
+    setDateRangeVal([]);
     onCancel();
   };
 
@@ -91,20 +87,12 @@ export default function TargetModal(props: any) {
           style={{ color: colorTextSecondary }}
         />
         <div className="text-[16px] font-[600] mb-[24px]">本次目标名称</div>
-        <Form
-          name="nameForm"
-          layout="vertical"
-          form={nameForm}
-          className="nameForm"
-        >
-          <Form.Item
-            label="目标名称"
-            name="name"
-            rules={[{ required: true }, { max: 64 }]}
-          >
-            <Input placeholder="请输入本次目标名称" className="w-[400px]" />
-          </Form.Item>
-        </Form>
+        <Input
+          placeholder="请输入本次目标名称"
+          className="w-[400px]"
+          value={nameVal}
+          onChange={onNameChange}
+        />
       </div>
     ),
     2: (
@@ -114,24 +102,14 @@ export default function TargetModal(props: any) {
           style={{ color: colorTextSecondary }}
         />
         <div className="text-[16px] font-[600] mb-[24px]">目标金额</div>
-        <Form
-          name="amountForm"
-          layout="vertical"
-          form={amountForm}
-          className="amount"
-        >
-          <Form.Item
-            label="目标金额"
-            name="amount"
-            rules={[{ required: true }]}
-          >
-            <InputNumber
-              placeholder="请输入本次目标金额"
-              step={1000}
-              className="w-[400px]"
-            />
-          </Form.Item>
-        </Form>
+        <InputNumber
+          placeholder="请输入本次目标金额"
+          step={1000}
+          min={0}
+          value={amountVal}
+          onChange={onAmountChange}
+          className="w-[400px]"
+        />
       </div>
     ),
     3: (
@@ -140,19 +118,89 @@ export default function TargetModal(props: any) {
           className="mb-[16px] text-[24px]"
           style={{ color: colorTextSecondary }}
         />
-        <div className="text-[16px] font-[600] mb-[24px]">目标金额</div>
-        <Form
-          name="timeForm"
-          layout="vertical"
-          form={timeForm}
-          className="timeForm"
-        >
-          <Form.Item label="目标时间" name="time" rules={[{ required: true }]}>
-            <Input />
-          </Form.Item>
-        </Form>
+        <div className="text-[16px] font-[600] mb-[24px]">目标期限</div>
+        <div className="flex justify-between gap-x-[62px]">
+          <div
+            className="w-[130px] flex justify-center items-center rounded cursor-pointer flex-col p-4"
+            style={{
+              transition: ".5s all",
+              border: `2px solid ${
+                timeType === "range" ? colorPrimary : colorBorderSecondary
+              }`,
+            }}
+            onClick={() => setTimeType("range")}
+          >
+            <svg
+              className="icon"
+              aria-hidden="true"
+              style={{
+                color: timeType === "range" ? colorPrimary : colorTextSecondary,
+                fontSize: 28,
+              }}
+            >
+              <use xlinkHref="#icon-countDown"></use>
+            </svg>
+            <div className="mt-[8px]">定期目标</div>
+          </div>
+          <div
+            className="w-[130px] flex justify-center items-center rounded-lg cursor-pointer flex-col p-4"
+            style={{
+              transition: ".5s all",
+              border: `2px solid ${
+                timeType === "forever" ? colorPrimary : colorBorderSecondary
+              }`,
+            }}
+            onClick={() => setTimeType("forever")}
+          >
+            <svg
+              className="icon"
+              aria-hidden="true"
+              style={{
+                color:
+                  timeType === "forever" ? colorPrimary : colorTextSecondary,
+                fontSize: 28,
+              }}
+            >
+              <use xlinkHref="#icon-funds-box-fill"></use>
+            </svg>
+            <div className="mt-[8px]">长期目标</div>
+          </div>
+        </div>
+        {timeType === "range" ? (
+          <RangePicker
+            value={dateRangeVal}
+            onChange={onRangeChange}
+            className="w-[322px] mt-[32px]"
+          />
+        ) : (
+          <DatePicker
+            onChange={onDateChange}
+            value={dateVal}
+            className="w-[322px] mt-[32px]"
+          />
+        )}
       </div>
     ),
+  };
+
+  const okDisableStaus = () => {
+    if (barWidth === 1 && !nameVal && nameVal === "") {
+      return true;
+    } else if (barWidth === 2 && !amountVal) {
+      return true;
+    } else if (barWidth === 3) {
+      if (timeType === "range") {
+        if (!dateRangeVal || !dateRangeVal.length) {
+          return true;
+        }
+      } else {
+        if (!dateVal) {
+          return true;
+        }
+      }
+    } else {
+      return false;
+    }
   };
 
   return (
@@ -161,16 +209,20 @@ export default function TargetModal(props: any) {
       onOk={onNext}
       onCancel={onClose}
       cancelButtonProps={{ onClick: onPre }}
-      destroyOnClose
       width={920}
       title={isShowEdit.idEdit ? "编辑目标" : "新建目标"}
       okText={barWidth === 3 ? "创建" : "下一步"}
       cancelText={barWidth === 1 ? "取消" : "上一步"}
+      okButtonProps={{ disabled: okDisableStaus() }}
     >
       <div className="mt-[24px]">
         <div
-          className="h-[3px] transition duration-600"
-          style={{ background: colorSuccess, width: barEnum[barWidth] }}
+          className="h-[3px]"
+          style={{
+            background: colorSuccess,
+            width: barEnum[barWidth],
+            transition: ".6s all",
+          }}
         />
       </div>
 
--- Front/src/pages/guidePage/step_2/index.tsx
+++ Front/src/pages/guidePage/step_2/index.tsx
@@ -1,54 +1,139 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import { Button, Checkbox, Form, Input, message, theme, Select, Space } from 'antd'
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  message,
+  theme,
+  Select,
+  Space,
+} from "antd";
 
-import { createFromIconfontCN } from '@ant-design/icons';
+import { createFromIconfontCN } from "@ant-design/icons";
 
-import { provinceData, cityData } from '../config'
+import { provinceData, cityData } from "../config";
 
-import { useGuideInfoModel } from '../../../models/guide'
+import { useGuideInfoModel } from "../../../models/guide";
 
-import '../index.less';
+import "../index.less";
 
 export default function Step2(props: any) {
-    const [status, setStatus] = useState<string>();
+  const [status, setStatus] = useState<string>();
 
-    const { guideInfo, setGuideInfo } = useGuideInfoModel();
-    const { onStepChange, setBarWidth } = props;
+  const { guideInfo, setGuideInfo } = useGuideInfoModel();
+  const { onStepChange, setBarWidth } = props;
 
-    const {
-        token: { colorBorderSecondary, colorPrimary, colorText, colorTextSecondary, colorTextTertiary },
-    } = theme.useToken();
+  const {
+    token: {
+      colorBorderSecondary,
+      colorPrimary,
+      colorText,
+      colorTextSecondary,
+      colorTextTertiary,
+    },
+  } = theme.useToken();
 
-    const onPreStep = () => {
-        setBarWidth('25%')
-        onStepChange(1)
-    }
+  const onPreStep = () => {
+    setBarWidth("25%");
+    onStepChange(1);
+  };
 
-    const onNextStep = (step: number) => {
-        setBarWidth('75%')
-        setGuideInfo({ ...guideInfo, step2: { status } })
-        onStepChange(step)
-    }
+  const onNextStep = (step: number) => {
+    setBarWidth("75%");
+    setGuideInfo({ ...guideInfo, step2: { status } });
+    onStepChange(step);
+  };
 
-    return (
-        <div className="guide-content-wrap">
-            <div className="guide-step-icon">
-                <svg className="icon" aria-hidden="true" style={{ color: colorTextSecondary, fontSize: 38 }}><use xlinkHref="#icon-shenfenleibie"></use></svg>
-            </div>
-            <div className="content-title" style={{ color: colorText }}>您是学生 OR 职场人 ? </div>
-            <div className="content-desc" style={{ color: colorTextTertiary, marginBottom: 14 }}>系统将根据您的身份信息向您提供行业建议、专业分析、就业形势等基础信息服务。</div>
-            <div className="content-place-step2">
-                <div className='student' style={status === 'student' ? { borderColor: colorPrimary } : {borderColor: colorBorderSecondary}} onClick={() => setStatus('student')}><div className='item-icon'><svg className="icon" aria-hidden="true" style={{ color: colorTextSecondary, fontSize: 28 }}><use xlinkHref="#icon-xuesheng"></use></svg></div><div className='item-title' style={{ color: colorText }}>学生党</div></div>
-                <div className='work' style={status === 'work' ? { borderColor: colorPrimary } : {borderColor: colorBorderSecondary}} onClick={() => setStatus('work')}><div className='item-icon'><svg className="icon" aria-hidden="true" style={{ color: colorTextSecondary, fontSize: 38 }}><use xlinkHref="#icon-gongwenbao"></use></svg></div><div className='item-title' style={{ color: colorText }}>打工人</div></div>
-            </div>
-
-            <div className="content-btn">
-                <Button type="primary" style={{ width: '100%', height: 44 }} onClick={() => onNextStep(3)} disabled={status ? false: true}>下一步</Button>
-                <Button type="text" style={{ width: '100%', height: 44, marginTop: 14 }} onClick={onPreStep}>返回</Button>
-            </div>
-
-            <div className="footer" style={{ color: colorTextTertiary }}>您的个人信息是保密的,我们不会泄露您的个人信息,仅引用于个性化推荐等基础服务</div>
+  return (
+    <div className="guide-content-wrap">
+      <div className="guide-step-icon">
+        <svg
+          className="icon"
+          aria-hidden="true"
+          style={{ color: colorTextSecondary, fontSize: 38 }}
+        >
+          <use xlinkHref="#icon-shenfenleibie"></use>
+        </svg>
+      </div>
+      <div className="content-title" style={{ color: colorText }}>
+        您是学生 OR 职场人 ?{" "}
+      </div>
+      <div
+        className="content-desc"
+        style={{ color: colorTextTertiary, marginBottom: 14 }}
+      >
+        系统将根据您的身份信息向您提供行业建议、专业分析、就业形势等基础信息服务。
+      </div>
+      <div className="content-place-step2">
+        <div
+          className="student"
+          style={
+            status === "student"
+              ? { borderColor: colorPrimary }
+              : { borderColor: colorBorderSecondary }
+          }
+          onClick={() => setStatus("student")}
+        >
+          <div className="item-icon">
+            <svg
+              className="icon"
+              aria-hidden="true"
+              style={{ color: colorTextSecondary, fontSize: 28 }}
+            >
+              <use xlinkHref="#icon-xuesheng"></use>
+            </svg>
+          </div>
+          <div className="item-title" style={{ color: colorText }}>
+            学生党
+          </div>
         </div>
-    )
+        <div
+          className="work"
+          style={
+            status === "work"
+              ? { borderColor: colorPrimary }
+              : { borderColor: colorBorderSecondary }
+          }
+          onClick={() => setStatus("work")}
+        >
+          <div className="item-icon">
+            <svg
+              className="icon"
+              aria-hidden="true"
+              style={{ color: colorTextSecondary, fontSize: 38 }}
+            >
+              <use xlinkHref="#icon-gongwenbao"></use>
+            </svg>
+          </div>
+          <div className="item-title" style={{ color: colorText }}>
+            打工人
+          </div>
+        </div>
+      </div>
+
+      <div className="content-btn">
+        <Button
+          type="primary"
+          style={{ width: "100%", height: 44 }}
+          onClick={() => onNextStep(3)}
+          disabled={status ? false : true}
+        >
+          下一步
+        </Button>
+        <Button
+          type="text"
+          style={{ width: "100%", height: 44, marginTop: 14 }}
+          onClick={onPreStep}
+        >
+          返回
+        </Button>
+      </div>
+
+      <div className="footer" style={{ color: colorTextTertiary }}>
+        您的个人信息是保密的,我们不会泄露您的个人信息,仅引用于个性化推荐等基础服务
+      </div>
+    </div>
+  );
 }
